0919cf5d50d22c697156aada27dbf756c6e43b67
Matias Paverini <matias.paverini@tsoftlatam.com>
2020-11-19 23:46:23 -0300

    Compilación con problemas

--- src/app/servicios/fcm.service.ts
+++ src/app/servicios/fcm.service.ts
@@ -12,9 +12,14 @@ import { HttpClient, HttpHeaders } from "@angular/common/http";
 export class FcmService {
 
   constructor(private fcm: FCM,
-     private authService: AuthenticationService, 
-     private firestore: FirestoreService, 
-     private http: HttpClient) { }
+    private authService: AuthenticationService,
+    private firestore: FirestoreService,
+    private http: HttpClient) { }
+
+  headers = new HttpHeaders({
+    'Content-Type': "application/json",
+    'Authorization': 'key=AAAAq8JVHAk:APA91bFzYee6QHfW1bmETVahitxckTwX2-41OAMI5AM6gtQiaI1PUf_K-xQwwkfO0uPA7nNNGMF6McmHf-KSDaCyyuq-4G4bCd761yOBekgXjd0AkyFovzfZr6ck6rMpa-iKGK_HpyaO'
+  })
 
   getToken() {
     this.fcm.getToken().then(token => {
@@ -34,7 +39,7 @@ export class FcmService {
       })
     };
 
-    return this.firestore.setData('dispositivos', data, data.userId);
+    return this.firestore.setData('Dispositivos', data, data.userId);
   }
 
   // notificationSetup() {
@@ -49,25 +54,25 @@ export class FcmService {
   //     })
   // }
 
-  getTokensByProfile(userProfile){
-      return new Promise((resolve) => { 
-        this.firestore.getUsuarios().subscribe(users => {
-          new Promise((resolve) => resolve(users.map(user => user.payload.doc.data() as Usuario).filter(user => user.Perfil == userProfile)))
+  getTokensByProfile(userProfile) {
+    return new Promise((resolve) => {
+      this.firestore.getUsuarios().subscribe(users => {
+        new Promise((resolve) => resolve(users.map(user => user.payload.doc.data() as Usuario).filter(user => user.Perfil == userProfile)))
           .then((usersByProfile: any[]) => {
             this.firestore.getDevices('Dispositivos').subscribe(devices => {
               let devicesByProfile = devices.map(device => device.payload.doc.data() as any)
-                                            .filter(device => usersByProfile.some(user => user.id == device.userId))
-                                            .map(device => device.token);
-              if(usersByProfile.length > 0)
+                .filter(device => usersByProfile.some(user => user.id == device.userId))
+                .map(device => device.token);
+              if (usersByProfile.length > 0)
                 resolve(devicesByProfile);
             });
           });
-        });
+      });
     });
   }
 
-  getTokensById(id){
-    return new Promise((resolve) => { 
+  getTokensById(id) {
+    return new Promise((resolve) => {
       this.firestore.getOne('Dispositivos', id).then(device => {
         let deviceById = device.data().token;
         resolve(deviceById);
@@ -77,21 +82,21 @@ export class FcmService {
 
   sendNotification(title: string, message: string, to: unknown, redirectTo?: string) {
     let body = {
-      "notification":{
+      "notification": {
         "title": title,
         "body": message,
-        "sound":"default",
-        "click_action":"FCM_PLUGIN_ACTIVITY",
-        "icon":"fcm_push_icon"
+        "sound": "default",
+        "click_action": "FCM_PLUGIN_ACTIVITY",
+        "icon": "fcm_push_icon"
       },
-      "data":{
+      "data": {
         "redirectTo": redirectTo
       },
-        //Un solo ID, topico o grupo
-        //"to": to,
-        // Multiples IDs
-        "registration_ids": to,
-        "priority":"high"
+      //Un solo ID, topico o grupo
+      //"to": to,
+      // Multiples IDs
+      "registration_ids": to,
+      "priority": "high"
     }
 
     return this.http.post("https://fcm.googleapis.com/fcm/send", body, { headers: this.headers }).subscribe();
